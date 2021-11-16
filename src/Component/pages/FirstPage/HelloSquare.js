a23e6ecafa172fa76f3a198da72dcb1ffe90a6e5
Idir6380 <87910077+Idir6380@users.noreply.github.com>
2021-11-16 12:24:27 +0100

    Add files via upload

--- src/Component/pages/FirstPage/HelloSquare.js
+++ src/Component/pages/FirstPage/HelloSquare.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./HelloSquare.css";
-import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 function HelloSquare() {
-  return (
-    <div className="hello-square">
-      <h1>
-        <span>Hel</span>lo
-      </h1>
-      <p>Welcome to our portfolio</p>
-    </div>
-  );
+    return (
+        <div className="hello-square">
+            <h1>
+                <span>Hel</span>lo
+            </h1>
+            <p>Welcome to our<br /> portfolio</p>
+        </div>
+    );
 }
 
 export default HelloSquare;
